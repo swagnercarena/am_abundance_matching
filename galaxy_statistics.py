eed50604b2f209be9f978b2a4e359b4665b7f6e8
Sebastian Wagner-Carena <swagnerc@stanford.edu>
2019-08-26 11:09:11 -0700

    trying to do the luminosity function bit right

--- galaxy_statistics.py
+++ galaxy_statistics.py
@@ -9,12 +9,13 @@ custom_blues = ["#66CCFF", "#33BBFF", "#00AAFF", "#0088CC", "#006699", "#004466"
 custom_blues_complement = ["#FF9966", "#FF7733", "#FF5500", "#CC4400", "#993300",
  "#662200"]
 
-def generate_wp(lf,halos,af_criteria,r_p_data,box_size,mag_cuts,pimax=40.0,
+def generate_wp(lf_list,halos,af_criteria,r_p_data,box_size,mag_cuts,pimax=40.0,
 	nthreads=1, scatters=None, deconv_repeat = 20, verbose=False):
 	"""	Generate the projected 2D correlation by abundance matching galaxies
 		Parameters:
-			lf: The luminosity function. The first column is the magnitudes and the
-				second column is the density in units of 1/Mpc^3.
+			lf_list: A list of luminosity functions for each mag_cut. The first 
+				column is the magnitudes and thesecond column is the density in 
+				units of 1/Mpc^3.
 			halos: A catalog of the halos in the n-body sim that can be indexed
 				into using the quantity name.
 			af_criteria: The galaxy property (i.e. vpeak) to use for abundance 
@@ -33,73 +34,76 @@ def generate_wp(lf,halos,af_criteria,r_p_data,box_size,mag_cuts,pimax=40.0,
 		Returns:
 			w_p(r_p) at the r_p values specified by r_p_data.
 	"""
-
-	# Initialize abundance function and calculate the number density of the
-	# halos in the box
-	af = AbundanceFunction(lf[:,0], lf[:,1], (-25, -5))
-	nd_halos = calc_number_densities(halos[af_criteria], box_size)
-	if scatters is not None:
-		remainders = []
-		for scatter in scatters:
-			remainders.append(
-				af.deconvolute(scatter*LF_SCATTER_MULT, deconv_repeat))
-
-	# If verbose output the match between abundance function and input data
-	if verbose:
-		matplotlib.rcParams.update({'font.size': 18})
-		plt.figure(figsize=(10,8))
-		plt.plot(lf[:,0], lf[:,1],lw=7,c=custom_blues[1])
-		x = np.linspace(np.min(lf[:,0])-2, np.max(lf[:,0])+2, 101)
-		plt.semilogy(x, af(x),lw=3,c=custom_blues[4])
-		plt.xlim([np.max(lf[:,0])+2,np.min(lf[:,0])])
-		plt.ylim([1e-5,1])
-		plt.xlabel('Magnitude (M - 5 log h)')
-		plt.ylabel('Number Density (1/ (Mpc^3 h))')
-		plt.legend(['Input','Fit'])
-		plt.title('Luminosity Function')
-		plt.yscale('log')
-		plt.show()
-
-	# Plot remainder to ensure the deconvolution returned reasonable results
-	if verbose and scatters is not None:
-		f, ax = plt.subplots(2,1, sharex='col', sharey='row', figsize=(15,12), 
-			gridspec_kw={'height_ratios':[2, 1]})
-
-		x, nd = af.get_number_density_table()
-		ax[0].plot(x, nd,lw=3,c=custom_blues[4])
-		legend = []
-		for scatter in scatters:
-			ax[0].plot(af._x_deconv[float(scatter*LF_SCATTER_MULT)],nd,lw=3,
-				c=custom_blues_complement[2*len(legend)])
-			legend.append('Scatter = %.2f'%(scatter))
-		ax[0].set_xlim([np.max(lf[:,0])+2,np.min(lf[:,0])])
-		ax[0].set_ylim([1e-5,1])
-		ax[0].set_ylabel('Number Density (1/ (Mpc^3 h))')
-		ax[0].legend(['Fit'] + legend)
-		ax[0].set_title('Deconvolved Luminosity Function')
-		ax[0].set_yscale('log')
-		ax[1].set_xlabel('Magnitude (M - 5 log h)')
-		ax[1].set_ylabel('(LF (deconv $\Rightarrow$ conv) - LF) / LF')
-		ax[1].set_xlim([np.max(lf[:,0])+2,np.min(lf[:,0])])
-		y_max = 0
-		for r_i in range(len(remainders)):
-			remainder = remainders[r_i]/nd
-			ax[1].plot(x, remainder,lw=3,
-				c=custom_blues_complement[2*r_i])
-			y_max = max(y_max,np.max(remainder[x>np.min(lf[:,0])]))
-		ax[1].set_ylim([-1.2,y_max*1.2])
-		plt.show()
-
-	# Conduct the abundance matching
-	catalogs = []
-	if scatters is not None:
-		for scatter in scatters:
-			catalogs.append(af.match(nd_halos, scatters[0]*LF_SCATTER_MULT))
-	else:
-		catalogs = [af.match(nd_halos)]
-
+	# Repeat once for each magnitude cut
 	wp_binneds = []
-	for mag_cut in mag_cuts:
+	for mag_cut_i in range(len(mag_cuts)):
+		mag_cut = mag_cuts[mag_cut_i]
+		lf = lf_list[mag_cut_i]
+		
+		# Initialize abundance function and calculate the number density of the
+		# halos in the box
+		af = AbundanceFunction(lf[:,0], lf[:,1], (-25, -5))
+		nd_halos = calc_number_densities(halos[af_criteria], box_size)
+		if scatters is not None:
+			remainders = []
+			for scatter in scatters:
+				remainders.append(
+					af.deconvolute(scatter*LF_SCATTER_MULT, deconv_repeat))
+
+		# If verbose output the match between abundance function and input data
+		if verbose:
+			matplotlib.rcParams.update({'font.size': 18})
+			plt.figure(figsize=(10,8))
+			plt.plot(lf[:,0], lf[:,1],lw=7,c=custom_blues[1])
+			x = np.linspace(np.min(lf[:,0])-2, np.max(lf[:,0])+2, 101)
+			plt.semilogy(x, af(x),lw=3,c=custom_blues[4])
+			plt.xlim([np.max(lf[:,0])+2,np.min(lf[:,0])])
+			plt.ylim([1e-5,1])
+			plt.xlabel('Magnitude (M - 5 log h)')
+			plt.ylabel('Number Density (1/ (Mpc^3 h))')
+			plt.legend(['Input','Fit'])
+			plt.title('Luminosity Function')
+			plt.yscale('log')
+			plt.show()
+
+		# Plot remainder to ensure the deconvolution returned reasonable results
+		if verbose and scatters is not None:
+			f, ax = plt.subplots(2,1, sharex='col', sharey='row', figsize=(15,12), 
+				gridspec_kw={'height_ratios':[2, 1]})
+
+			x, nd = af.get_number_density_table()
+			ax[0].plot(x, nd,lw=3,c=custom_blues[4])
+			legend = []
+			for scatter in scatters:
+				ax[0].plot(af._x_deconv[float(scatter*LF_SCATTER_MULT)],nd,lw=3,
+					c=custom_blues_complement[2*len(legend)])
+				legend.append('Scatter = %.2f'%(scatter))
+			ax[0].set_xlim([np.max(lf[:,0])+2,np.min(lf[:,0])])
+			ax[0].set_ylim([1e-5,1])
+			ax[0].set_ylabel('Number Density (1/ (Mpc^3 h))')
+			ax[0].legend(['Fit'] + legend)
+			ax[0].set_title('Deconvolved Luminosity Function')
+			ax[0].set_yscale('log')
+			ax[1].set_xlabel('Magnitude (M - 5 log h)')
+			ax[1].set_ylabel('(LF (deconv $\Rightarrow$ conv) - LF) / LF')
+			ax[1].set_xlim([np.max(lf[:,0])+2,np.min(lf[:,0])])
+			y_max = 0
+			for r_i in range(len(remainders)):
+				remainder = remainders[r_i]/nd
+				ax[1].plot(x, remainder,lw=3,
+					c=custom_blues_complement[2*r_i])
+				y_max = max(y_max,np.max(remainder[x>np.min(lf[:,0])]))
+			ax[1].set_ylim([-1.2,y_max*1.2])
+			plt.show()
+
+		# Conduct the abundance matching
+		catalogs = []
+		if scatters is not None:
+			for scatter in scatters:
+				catalogs.append(af.match(nd_halos, scatters[0]*LF_SCATTER_MULT))
+		else:
+			catalogs = [af.match(nd_halos)]
+
 		wp_scatts = []
 		for catalog in catalogs:
 			# A luminosity cutoff to use for the correlation function.
