ef2aa7c15db87867b449bd43c922138ad2c94d04
Sebastian M. Wagner-Carena <swagnerc@rhel6-64f.slac.stanford.edu>
2019-09-30 19:05:56 -0700

    small local changes to galaxy function

--- galaxy_statistics.py
+++ galaxy_statistics.py
@@ -257,9 +257,10 @@ class AMLikelihood(object):
 		# Deconvolve the scatter and generate catalogs for each mag_cut
 		catalog_list = []
 		for af in self.af_list:
+			print(self.deconv_repeat)
 			af.deconvolute(scatter*LF_SCATTER_MULT,self.deconv_repeat)
 			catalog_list.append(af.match(nd_halos,scatter*LF_SCATTER_MULT,
-				do_rematch=False))
+				do_rematch=True))
 
 		log_like = 0
 		wp_saved_results = []
