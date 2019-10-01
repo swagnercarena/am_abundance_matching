2a43e061efb46587e359af2cfdd247210a657348
Sebastian Wagner-Carena <swagnerc@stanford.edu>
2019-10-01 14:02:03 -0700

    get rid of the psace just in case

--- run_emcee.bat
+++ run_emcee.bat
@@ -3,7 +3,6 @@
 #BSUB -R "span[hosts=1]"     
 #BSUB -eo /u/ki/swagnerc/abundance_matching/out_files/temp.err
 #BSUB -oo /u/ki/swagnerc/abundance_matching/out_files/temp.out
-
 source /afs/slac.stanford.edu/u/ki/swagnerc/bsub_startup.txt
 cd /afs/slac.stanford.edu/u/ki/swagnerc/abundance_matching/am_abundance_matching
-python emcee_script.py
+python emcee_script.py 
