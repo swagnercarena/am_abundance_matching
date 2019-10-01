c1f1a12e35cdd5c4acc0ed4ab452589535f131ab
Sebastian Wagner-Carena <swagnerc@stanford.edu>
2019-09-30 21:21:32 -0700

    .err not .er

--- run_emcee.bat
+++ run_emcee.bat
@@ -1,7 +1,7 @@
 #BSUB -W 1200
 #BSUB -n 10
 #BSUB -R "span[hosts=1]"     
-#BSUB -eo /u/ki/swagnerc/abundance_matching/out_files/temp.er
+#BSUB -eo /u/ki/swagnerc/abundance_matching/out_files/temp.err
 #BSUB -oo /u/ki/swagnerc/abundance_matching/out_files/temp.out
 source ~/bsub_startup.txt
 cd ~/abundance_matching/am_abundance_matching
