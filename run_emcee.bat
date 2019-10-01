48a322cc14eed9ea2b966309c5bf5379db132d27
Sebastian Wagner-Carena <swagnerc@stanford.edu>
2019-09-30 21:16:51 -0700

    one more time

--- run_emcee.bat
+++ run_emcee.bat
@@ -1,9 +1,9 @@
-#BSUB -W 1200                                                                                                                                                                         
-#BSUB -n 10                                                                                                                                                                         
+#BSUB -W 1200
+#BSUB -n 10
 #BSUB -R "span[hosts=1]"     
-#BSUB -eo /u/ki/swagnerc/abundance_matching/out_files/temp.err                                                                                                                                                     
-#BSUB -oo /u/ki/swagnerc//abundance_matching/out_files/temp.out                                                                                                                     
-                                                                                                                                                                                     
+#BSUB -eo /u/ki/swagnerc/abundance_matching/out_files/temp.er
+#BSUB -oo /u/ki/swagnerc/abundance_matching/out_files/temp.out
+
 source ~/.bashrc
 cd ~/abundance_matching/am_abundance_matching
 python emcee_script.py
