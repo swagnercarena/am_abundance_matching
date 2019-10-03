53776b13b1c829a8b0ce62ab555ba2e1fbcc5273
Sebastian Wagner-Carena <swagnerc@stanford.edu>
2019-10-02 17:14:15 -0700

    pooling does not help for some reason

--- emcee_script.py
+++ emcee_script.py
@@ -101,15 +101,12 @@ if exists(csv_path):
 	pos = frame.values[-n_walkers:]
 
 print(pos)
-
-with Pool() as pool:
-	sampler = emcee.EnsembleSampler(n_walkers, n_params, 
-		like_class.log_likelihood,pool=pool)
-	with open(csv_path, 'a',1) as f:
-		writer = csv.writer(f)
-		if not exists(csv_path):
-			writer.writerow(fields)
-		save_step = 1
-		for step in tqdm(range(n_steps//save_step+1)):
-			pos, _, _ = sampler.run_mcmc(pos, save_step)
-			writer.writerows(sampler.chain[:,-save_step:,:].reshape(-1,n_params))
+sampler = emcee.EnsembleSampler(n_walkers, n_params, like_class.log_likelihood)
+with open(csv_path, 'a',1) as f:
+	writer = csv.writer(f)
+	if not exists(csv_path):
+		writer.writerow(fields)
+	save_step = 1
+	for step in tqdm(range(n_steps//save_step+1)):
+		pos, _, _ = sampler.run_mcmc(pos, save_step)
+		writer.writerows(sampler.chain[:,-save_step:,:].reshape(-1,n_params))
