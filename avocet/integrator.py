d12a159975d4237f94105578e118ae02e4e24fce
Sebastian Wagner-Carena <swagnerc@stanford.edu>
2020-10-29 20:24:03 -0700

    added a bunch of NFW and dynamical friction functions that were all working. NFW tests still need to be fixed to deal with unit change

--- avocet/integrator.py
+++ avocet/integrator.py
@@ -1,8 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Functions for integrating a subhalo within a host potential
+
+Units: In order to avoid numerical issues in the calculation, the units have
+	been chosen to work well for objects ranging in mass from 1e9-1e15 M_sun.
+	Therefore the units are as follows:
+	Mass - 1e13 M_sun
+	Radius - 300 kpc
+	Velocity - 400 km/s
+	Time - By virtue of the other units, time is in units of s*300kpc/400km
+		or 2.31425819e16 seconds
+
+TODO:
+	1)	Change the units being used to something more natural and reflect
+		that in the comments
+	2)	Include variable force softening scale in the integrator codes
+	3)	Write code to allow for multiple NFWs?
+	4) 	Discuss unit choices and purpose in an opening statement.
+"""
+
 import numba
 import numpy as np
+import math
 
-# Declare some global variables
-G = 6.67430e-11
+# Declare some global variables (400 km^2*300 kpc/(1e13 M_sun*s^2))
+G = 0.8962419740798497
 
 
 @numba.njit()
@@ -43,7 +65,7 @@ def leapfrog_v_step(v0,dt,a0,a1):
 
 @numba.njit()
 def calc_neg_grad_nb(part_pos,part_mass,pos):
-	""" Calculate the negative gradient of the n-body potential at a specific
+	"""	Calculate the negative gradient of the n-body potential at a specific
 		position.
 
 		Parameters:
@@ -90,8 +112,8 @@ def leapfrog_int_nb(part_pos,part_vel,part_mass,dt,num_dt,save_pos_array,
 	# Number of particles
 	num_p = len(part_pos)
 
-	# We need to store the current potential for the
-	# particles before conducting the update
+	# We need to store the current potential for the particles before conducting
+	# the update
 	a0_vec = np.zeros((num_p,3),dtype=np.float64)
 
 	# Variables we'll use for inplace calculation
@@ -153,7 +175,7 @@ def leapfrog_int_nb(part_pos,part_vel,part_mass,dt,num_dt,save_pos_array,
 
 @numba.njit()
 def calc_neg_grad_nfw(rho_0,r_scale,pos_nfw,pos):
-	""" Calculate the negative gradient of the nfw potential at a specific
+	"""	Calculate the negative gradient of the nfw potential at a specific
 		position.
 
 		Parameters:
@@ -171,3 +193,96 @@ def calc_neg_grad_nfw(rho_0,r_scale,pos_nfw,pos):
 	r_hat = (pos_nfw-pos)/r
 
 	return norm*(1/r2 * np.log(1+r/r_scale) - 1/(r_scale+r)*1/r) * r_hat
+
+
+@numba.njit()
+def leapfrog_int_nfw(pos_init,vel_init,rho_0,r_scale,pos_nfw_array,dt,
+	save_pos_array,save_vel_array):
+	"""	Integrate rotation through an NFW potential
+
+		Parameters:
+			pos_init (np.array): 3D initial position of the particle. Will
+				be modified.
+			vel_init (np.array): 3D initial velocity of the particle. Will
+				be modified.
+			rho_0 (float): The amplitude of the NFW (in unites of mass)
+			r_scale (float): The scale radius of the NFW
+			pos_nfw_array (np.array): The num_dt*3D position of the NFW at each
+				time step. This array will be used to determine the number of
+				integration steps.
+			dt (float): The size of a timestep
+			save_pos_array (np.array): A (num_dt+1)*N*3D array where the
+				positions will be saved.
+			save_vel_array (np.array): A (num_dt+1)*N*3D array where the
+				velocities will be saved.
+	"""
+	# Clear save arrays in case they have values
+	save_pos_array *= 0
+	save_vel_array *= 0
+	a0 = np.zeros(3,dtype=np.float64)
+	a1 = np.zeros(3,dtype=np.float64)
+	num_dt = len(pos_nfw_array)
+
+	for ti in range(num_dt):
+		# Save the pos and vel at this step
+		save_pos_array[ti] += pos_init
+		save_vel_array[ti] += vel_init
+
+		# First get the force for the particle at the current position
+		a0 += calc_neg_grad_nfw(rho_0,r_scale,pos_nfw_array[ti],pos_init)
+
+		# Do the fist integration step.
+		leapfrog_p_step(pos_init,vel_init,dt,a0)
+
+		# Calculate the force on the particle at the new position.
+		a1 += calc_neg_grad_nfw(rho_0,r_scale,pos_nfw_array[ti],pos_init)
+
+		# Do a step of leapfrog integration on this particle
+		leapfrog_v_step(vel_init,dt,a0,a1)
+
+		# Reset the force vectors
+		a0 *= 0
+		a1 *= 0
+
+	# Save the pos and vel for the final step
+	save_pos_array[num_dt] += pos_init
+	save_vel_array[num_dt] += vel_init
+
+
+"""-----------------Dynamical Friction + NFW Functions----------------------"""
+
+
+@numba.njit()
+def calc_neg_grad_f_dyn(vel,rho,m_sub,m_host,sigma_v):
+	"""	Calculate the negative gradient caused by dynamical friction in the
+		potential.
+
+		Parameters:
+			vel (np.array): The 3D velocity of the substructure in question
+			rho (float): The matter density at the position of the
+				substructure
+			m_sub (float): The mass of the substructure
+			m_host (float): The mass of the host halo
+			sigma_v (float): The velocity dispersion of the halo at the
+				position of the substructure.
+
+		Returns:
+			(np.array): The 3D negative gradient of the dynamical
+				friction 'potential'.
+	"""
+	# Calculate some velocity qunatities we will use.
+	v_2 = np.sum(np.square(vel))
+	v_mag = np.sqrt(v_2)
+
+	# First calculate the Coulomb logarithm
+	ln_lambda = np.log(m_host) - np.log(m_sub)
+
+	# Calculate the dynamical friction variable X
+	x = v_mag / (np.sqrt(2)*sigma_v)
+
+	# Put everything together into our dynamical friction term
+	a_dyn = vel/(v_2*v_mag)
+	a_dyn *= -4*np.pi*G**2*m_sub*rho*ln_lambda
+	a_dyn *=(math.erf(x)-2*x/np.sqrt(np.pi)*np.exp(-x**2))
+
+	return a_dyn
--- test/integrator_test.py
+++ test/integrator_test.py
@@ -1,7 +1,14 @@
 import unittest
 from avocet import integrator
 import numpy as np
+from galpy.orbit import Orbit
 from galpy.potential import NFWPotential
+import math
+
+# Some useful conversion factors we'll use
+M_sun2kg = 1.9891e30*1e13
+km2m = 400*1e3
+kpc2m = 300*3.086e19
 
 
 class IntegratorTestsNB(unittest.TestCase):
@@ -53,23 +60,24 @@ class IntegratorTestsNB(unittest.TestCase):
 		part_pos = np.array([[0,1,0],[0,0,2]])
 		part_mass = np.array([3,4],dtype=np.float64)
 
-		g_comp = - G*part_mass[0]/np.sqrt(2)*np.array([1,-1,0])
-		g_comp += - G*part_mass[1]/np.sqrt(5)*np.array([1,0,-2])
+		g_comp = - G*part_mass[0]*M_sun2kg/2**1.5*np.array([1,-1,0])
+		g_comp += - G*part_mass[1]*M_sun2kg/5**1.5*np.array([1,0,-2])
+		g_comp /= kpc2m*km2m**2
 		g = integrator.calc_neg_grad_nb(part_pos,part_mass,pos)
-		np.testing.assert_almost_equal(g,g_comp)
+		np.testing.assert_almost_equal(g,g_comp,decimal=6)
 
 	def test_leapfrog_int_nb(self):
 		# Try some analytically known 2 body integrals and ensure that the
 		# results match expectations.
-		G = 6.67430e-11
-		M = 1e10
+		G = 0.8962419740798497
+		M = 1
 		r = 1
 		part_pos = np.array([[0,0,0],[0,r,0]],dtype=np.float64)
-		part_vel = np.array([[0,0,0],[np.sqrt(G*M),0,0]],dtype=np.float64)
-		part_mass = np.array([M,1],dtype=np.float64)
+		part_vel = np.array([[0,0,0],[np.sqrt(G*M/r),0,0]],dtype=np.float64)
+		part_mass = np.array([M,1e-6],dtype=np.float64)
 
 		dt = 0.001
-		period = 2*np.pi*np.sqrt(r**3/(G*M))
+		period = 2*np.pi*r/np.sqrt(G*M/r)
 		num_dt = int(period/dt)
 		save_pos_array = np.zeros((num_dt+1,2,3))
 		save_vel_array = np.zeros((num_dt+1,2,3))
@@ -78,11 +86,10 @@ class IntegratorTestsNB(unittest.TestCase):
 			save_pos_array,save_vel_array)
 
 		for i in range(2):
-			for j in range(2):
-				np.testing.assert_almost_equal(save_pos_array[0,i,j],
-					save_pos_array[-1,i,j],decimal=3)
-				np.testing.assert_almost_equal(save_vel_array[0,i,j],
-					save_vel_array[-1,i,j],decimal=3)
+			np.testing.assert_almost_equal(save_pos_array[0,i,:],
+				save_pos_array[-1,i,:],decimal=3)
+			np.testing.assert_almost_equal(save_vel_array[0,i,:],
+				save_vel_array[-1,i,:],decimal=3)
 
 		# Quickly repeat the same for 5 more periods
 		num_dt = int(5*period/dt)
@@ -92,19 +99,18 @@ class IntegratorTestsNB(unittest.TestCase):
 			save_pos_array,save_vel_array)
 
 		for i in range(2):
-			for j in range(2):
-				np.testing.assert_almost_equal(save_pos_array[0,i,j],
-					save_pos_array[-1,i,j],decimal=3)
-				np.testing.assert_almost_equal(save_vel_array[0,i,j],
-					save_vel_array[-1,i,j],decimal=3)
+			np.testing.assert_almost_equal(save_pos_array[0,i,:],
+				save_pos_array[-1,i,:],decimal=3)
+			np.testing.assert_almost_equal(save_vel_array[0,i,:],
+				save_vel_array[-1,i,:],decimal=3)
 
 		# Now check that we get reasonable results for a e = 0.9 Kepplerian
 		# orbit
 		rp = 1.9
 		vt = 0.005927
 		period = 243
-		m1 = 1e2
-		m2 = 1e7
+		m1 = 1e2*6.67430e-11/0.8962419740798497
+		m2 = 1e7*6.67430e-11/0.8962419740798497
 		rcm = m1*rp/(m1+m2)
 		vcm = vt/rp*rcm
 
@@ -135,7 +141,7 @@ class IntegratorTestsNFW(unittest.TestCase):
 		# results as galpy (but faster hopefully :))
 		r_scale = 2
 		rho_0 = 1e10
-		G = 6.67430e-11
+		G = 0.8962419740798497
 		nfw = NFWPotential(a=r_scale,amp=rho_0*G*4*np.pi*r_scale**3)
 
 		pos_nfw = np.zeros(3,dtype=np.float64)
@@ -163,3 +169,121 @@ class IntegratorTestsNFW(unittest.TestCase):
 				np.testing.assert_almost_equal(
 					neg_grad/np.sqrt(np.sum(np.square(neg_grad))),
 					-pos/r)
+
+	def test_leapfrog_int_nfw(self):
+		# Test the circular orbits and circular orbits within a moving NFW work
+		# Start with something simple like circular motion
+		rho_0 = 1e11
+		r_scale = 1
+		G = 0.8962419740798497
+
+		dt = 0.0001
+		num_dt = int(1e5)
+
+		pos_nfw_array = np.tile(np.zeros(3,dtype=np.float64),(num_dt,1))
+		save_pos = np.zeros((num_dt+1,3))
+		save_vel = np.zeros((num_dt+1,3))
+
+		r_init = 20
+		pos_init = np.array([r_init,0,0],dtype=np.float64)
+
+		M_r = 4*np.pi*rho_0*r_scale**3*(np.log((r_scale+r_init)/r_scale)+
+			r_scale/(r_scale+r_init) - 1)
+		v_r = np.sqrt(G*M_r/r_init)
+		vel_init = np.array([0,v_r,0],dtype=np.float64)
+		period = 2*np.pi*np.sqrt(r_init**3/(G*M_r))
+		# Get the final completed period
+		last_period = int(int(num_dt*dt/period)*period//dt)
+
+		integrator.leapfrog_int_nfw(pos_init,vel_init,rho_0,r_scale,
+			pos_nfw_array,dt,save_pos,save_vel)
+		# Check that it returns to the original position once per period.
+		np.testing.assert_almost_equal(save_pos[0,:],save_pos[last_period,:],
+			decimal=3)
+
+		# Repeat the test with a moving NFW
+		pos_nfw_array = np.tile(np.zeros(3,dtype=np.float64),(num_dt,1))
+		for pi, pos in enumerate(pos_nfw_array):
+			pos[0] += pi*dt
+
+		pos_init = np.array([r_init,0,0],dtype=np.float64)
+		vel_init = np.array([1,v_r,0],dtype=np.float64)
+		integrator.leapfrog_int_nfw(pos_init,vel_init,rho_0,r_scale,
+			pos_nfw_array,dt,save_pos,save_vel)
+
+		np.testing.assert_almost_equal(save_pos[0,:],
+			save_pos[last_period,:]-pos_nfw_array[last_period,:],
+			decimal=3)
+
+	def test_leapfrog_nfw_galpy(self):
+		# Compare a more complicated set of initial conditions to the galpy
+		# outputs (which are slow but known to work).
+
+		# Set the parameters for a slightly offset circular orbit
+		rho_0 = 1e11
+		r_scale = 1
+		G = 0.8962419740798497
+
+		dt = 0.001
+		num_dt = int(3e4)
+
+		pos_nfw_array = np.tile(np.zeros(3,dtype=np.float64),(num_dt,1))
+		save_pos = np.zeros((num_dt+1,3))
+		save_vel = np.zeros((num_dt+1,3))
+
+		r_init = 20
+		pos_init = np.array([r_init,0,0],dtype=np.float64)
+
+		M_r = 4*np.pi*rho_0*r_scale**3*(np.log((r_scale+r_init)/r_scale)+
+			r_scale/(r_scale+r_init) - 1)
+		v_r = np.sqrt(G*M_r/r_init)
+		v_kick = 1e-1
+		vel_init = np.array([v_kick,v_r,0],dtype=np.float64)
+
+		integrator.leapfrog_int_nfw(pos_init,vel_init,rho_0,r_scale,
+			pos_nfw_array,dt,save_pos,save_vel)
+
+		# Compare to galpy orbits
+		o=Orbit([r_init,v_kick,v_r,0,0,0])
+		nfw = NFWPotential(a=r_scale,amp=rho_0*G*4*np.pi*r_scale**3)
+		ts = np.linspace(0,dt*num_dt,num_dt+1)
+		o.integrate(ts,nfw,method='leapfrog',dt=dt)
+
+		np.testing.assert_almost_equal(o.x(ts),save_pos[:,0])
+		np.testing.assert_almost_equal(o.y(ts),save_pos[:,1])
+		np.testing.assert_almost_equal(o.z(ts),save_pos[:,2])
+
+		# Make the kick bigger and see what happens
+		v_kick = 5e-1
+		pos_init = np.array([r_init,0,0],dtype=np.float64)
+		vel_init = np.array([v_kick,v_r,v_kick],dtype=np.float64)
+		integrator.leapfrog_int_nfw(pos_init,vel_init,rho_0,r_scale,
+			pos_nfw_array,dt,save_pos,save_vel)
+
+		# Do the same for galpy
+		o=Orbit([r_init,v_kick,v_r,0,v_kick,0])
+		nfw = NFWPotential(a=r_scale,amp=rho_0*G*4*np.pi*r_scale**3)
+		ts = np.linspace(0,dt*num_dt,num_dt+1)
+		o.integrate(ts,nfw,method='leapfrog',dt=dt)
+
+		np.testing.assert_almost_equal(o.x(ts),save_pos[:,0])
+		np.testing.assert_almost_equal(o.y(ts),save_pos[:,1])
+		np.testing.assert_almost_equal(o.z(ts),save_pos[:,2])
+
+	def test_calc_neg_grad_f_dyn(self):
+		# Here we can't compare to galpy since their implementation of lambda
+		# is different. Instead we have hand computed some correct dynamical
+		# friction values and we compare to that.
+		G = 0.8962419740798497
+		v = np.ones(3,dtype=np.float64)
+		m_sub = 1
+		m_host = np.exp(4)
+		rho = 1
+		sigma = 1
+		x = 1/(np.sqrt(2)*sigma)
+		x_factor = (math.erf(x)-2*x/np.sqrt(np.pi)*np.exp(-x**2))
+		ln_lamba = 4
+
+		hand_calc = -4*np.pi*G**2*m_sub/3*ln_lamba*x_factor*v/np.sqrt(3)
+		np.testing.assert_almost_equal(hand_calc,
+			integrator.calc_neg_grad_f_dyn(v,rho,m_sub,m_host,sigma))
